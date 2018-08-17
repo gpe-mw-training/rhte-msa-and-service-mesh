a35d4c0d61aca8b11b423c44088d78bf585ff959
Chad Darby <cdarby@redhat.com>
2018-08-16 20:53:22 -0400

    base coolstore-catalog

--- coolstore-gateway/coolstore-catalog/src/main/java/com/redhat/coolstore/catalog/model/package-info.java
+++
@@ -1,3 +0,0 @@
-@io.vertx.codegen.annotations.ModuleGen(groupPackage = "com.redhat.coolstore.catalog.model", name = "coolstore-catalog-model")
-package com.redhat.coolstore.catalog.model;
-
--- coolstore-gateway/coolstore-catalog/src/main/java/com/redhat/coolstore/catalog/verticle/service/package-info.java
+++
@@ -1,3 +0,0 @@
-@io.vertx.codegen.annotations.ModuleGen(groupPackage = "com.redhat.coolstore.catalog.verticle.service", name = "coolstore-catalog-service")
-package com.redhat.coolstore.catalog.verticle.service;
-
