9cabb388d142e20dcd125c9f6fd4c50d9c3c1258
Chad Darby <cdarby@redhat.com>
2018-09-27 12:18:09 -0400

    updated script to handle continuous loops

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-for i in $(seq 10); do
-  curl $GATEWAY_URL
-done
+# if no params passed in, just do a continuous/infinite loop
+# else if we have a param then it is number of requests
+
+numberOfRequests=$1
+
+if [ $# -eq 0 ]; then
+
+	while true; do 
+	  curl $GATEWAY_URL		
+	done
+else
+	let "i = 0"
+	while [ $i -lt $numberOfRequests ]; do	
+	  curl $GATEWAY_URL
+	  let "i=$((i + 1))"
+	done
+fi 
